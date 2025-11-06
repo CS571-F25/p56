5ff4eaa97c66f0b15cedcd124bf6b5742961e977
Hassan Murayr <murayr@wisc.edu>
2025-11-05 18:32:38 -0600

    initital publish part two

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,5 @@
+export default function AboutMe (props) {
+    return <div>
+        <h1>Hassan Murayr</h1>
+    </div>
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home (props) {
+    return <div>
+        <h1>Home!</h1>
+		<h2>Coming soon! This will be an adaptation of NEOM's website</h2>
+    </div>
+}
